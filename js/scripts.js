4f8e0066eb9358b8a55efeb99292d6da9a1a9320
Sara Kane <kane.sarak@gmail.com>
2020-06-09 14:25:54 -0700

    Update function to return true if year is divisible by 400
    Co-authored by: Kelly Eidsvik <kelly.eidsvik@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 function leapYear(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   }
   else {
     return false;
   }
-  
 }
 
 $(document).ready(function() {

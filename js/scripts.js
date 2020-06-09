02a350ba25a4146095f43c671291f792b13e510a
Sara Kane <kane.sarak@gmail.com>
2020-06-09 14:09:44 -0700

    Add function that returns false when checking if input is a leap year
    Co-authored by: Kelly Eidsvik <kelly.eidsvik@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function leapYear(year) {
+  return false;
+}
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    const year = parseInt($("input#year").val());
+    const result = leapYear(year);
+    $("#result").text(result);
+  });
+});
